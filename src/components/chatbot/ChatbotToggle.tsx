0ad13bcc5a1c43e0ae1697eee060310277f2d75e
beom710 <beom710@naver.com>
2026-01-15 17:44:17 +0900

    rucide icon add

--- src/components/chatbot/ChatbotToggle.tsx
+++ src/components/chatbot/ChatbotToggle.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { BotMessageSquare } from "lucide-react"
 
 interface ChatbotToggleProps {
   onClick: () => void
@@ -8,12 +9,12 @@ export default function ChatbotToggle({ onClick }: ChatbotToggleProps) {
   return (
     <div className="fixed right-4 bottom-6 z-40">
       <Button
-        size="lg"
-        className="rounded-full shadow-lg"
+        size="icon"
         onClick={onClick}
+        className="relative w-16 h-16 rounded-full shadow-lg"
       >
-        @@test@@
-      </Button>
+          <BotMessageSquare className="w-full h-full" />
+       </Button>
     </div>
   )
 }
